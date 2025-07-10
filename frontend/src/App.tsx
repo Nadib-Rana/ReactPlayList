822eb0c11b7e5f6ecdfcd75fa76dd7924e4aa280
Nadib Rana <codecrafersnadib@gmail.com>
2025-07-11 02:12:47 +0600

    Readme fill add

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+  return (
+    <>
+    <h1  className='text-green-500 text-center text-[25px]'>Hello Developer</h1>
+    <h1>{new Date().getTime()}</h1>
+    </>
+  )
+}
+
+export default App
