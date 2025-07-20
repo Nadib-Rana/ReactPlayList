2f8e9eb488013e4a9e1204b649d8942987eb0b5e
Nadib Rana <codecrafersnadib@gmail.com>
2025-07-20 23:11:57 +0600

    nadib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-
+import { useRef } from 'react';
 
 function App() {
-  
+  const headingRef = useRef<HTMLHeadingElement>(null);
+
+  const changeText = () => {
+  for (let i: number = 0; i < 3; i++) {
+    setTimeout(() => {
+      if (headingRef.current) {
+        headingRef.current.innerHTML = `${i}`;
+      }
+    }, i * 1000); // 1-second intervals
+  }
+};
+
   return (
     <>
-    <h1  className='text-green-500 text-center text-[25px]'>Hello Developer</h1>
-    <h1>{new Date().getTime()}</h1>
+      <h1 ref={headingRef} className="text-green-500 text-center text-[25px]">
+        Hello Developer
+      </h1>
+      <button onClick={changeText}>Change</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
